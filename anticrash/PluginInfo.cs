11cfc7621ed3892579bb1c7455a29889841979a3
Jaden Yoder <jadentyoder@gmail.com>
2023-10-26 19:15:51 -0400

    Add project files.

---
+++ anticrash/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace anticrash
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.author.gorillatag.anticrash";
+        public const string Name = "anticrash";
+        public const string Version = "1.0.0";
+    }
+}
